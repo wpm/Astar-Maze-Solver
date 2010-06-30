575d3496bb64d17f74b6ccaab2ba564a4a26655f
William McNeill <billmcn@gmail.com>
2010-06-30 13:04:50 -0700

    Use boost Namespace
    Move everything into the boost namespace.
    
    Write a few main() functions to verify that various valid expressions required by the A-star search do work.
    
    A-star search iteself still doesn't build.

--- grid.hpp
+++ grid.hpp
@@ -4,40 +4,24 @@
 #include <math.h>
 
 
-// Forward declarations
-namespace grid {
-  class graph;
-  struct edge_weight_map;
-}
-
-// ReadablePropertyGraph associated types
 namespace boost {
-  // This has to be declared outside the implicit_ring namespace block so
-  // that the namespaces do not nest.
-  template<>
-  struct property_map<grid::graph,
-                      edge_weight_t> {
-    typedef grid::edge_weight_map type;
-    typedef grid::edge_weight_map const_type;
-  };
-}
+// Forward declarations
+  struct edge_weight_map;
 
-
-namespace grid {
   // A non-wrapping rank 2 grid
   #define RANK 2
-  typedef boost::array<std::size_t, RANK> dimension_array;
-  class graph:public boost::grid_graph<RANK> {
+  typedef array<std::size_t, RANK> dimension_array;
+  class barrier_grid:public grid_graph<RANK> {
   public:
     // Types required by a-star
     typedef vertex_descriptor vertex_property_type;
     typedef vertices_size_type vertex_index;
     
-    graph(dimension_array& d):boost::grid_graph<RANK>(d) {};
+    barrier_grid(dimension_array& d):grid_graph<RANK>(d) {};
   };
 
-  typedef boost::graph_traits<graph>::vertex_descriptor vertex_descriptor;
-  typedef boost::graph_traits<graph>::edge_descriptor edge_descriptor;
+  typedef graph_traits<barrier_grid>::vertex_descriptor vertex_descriptor;
+  typedef graph_traits<barrier_grid>::edge_descriptor edge_descriptor;
 
 
   /*
@@ -47,25 +31,33 @@ namespace grid {
     typedef float value_type;
     typedef float reference;
     typedef edge_descriptor key_type;
-    typedef boost::readable_property_map_tag category;
+    typedef readable_property_map_tag category;
 
     reference operator[](key_type e) const {
       // All edges have a weight of one.
       return 1;
     }
   };
+
+
+  // ReadablePropertyGraph associated types
+  template<>
+  struct property_map<barrier_grid,
+                      edge_weight_t> {
+    typedef edge_weight_map type;
+    typedef edge_weight_map const_type;
+  };
   
-  typedef boost::property_map<graph,
-                              boost::edge_weight_t>::const_type
+  typedef property_map<barrier_grid, edge_weight_t>::const_type
           const_edge_weight_map;
-  typedef boost::property_traits<const_edge_weight_map>::reference
+  typedef property_traits<const_edge_weight_map>::reference
           edge_weight_map_reference;
-  typedef boost::property_traits<const_edge_weight_map>::key_type
+  typedef property_traits<const_edge_weight_map>::key_type
           edge_weight_map_key;
   
   // PropertyMap valid expressions
   edge_weight_map_reference get(const_edge_weight_map, edge_weight_map_key);
-
+  
   inline edge_weight_map_reference
   get(const_edge_weight_map pmap, edge_weight_map_key e) {
     return pmap[e];
@@ -73,19 +65,18 @@ namespace grid {
 
 
   // ReadablePropertyGraph valid expressions
-  const_edge_weight_map get(boost::edge_weight_t, const graph&);
-
-  inline const_edge_weight_map
-  get(boost::edge_weight_t, const graph& g) {
+  const_edge_weight_map get(edge_weight_t, const barrier_grid&);
+  
+  inline const_edge_weight_map get(edge_weight_t, const barrier_grid& g) {
     return const_edge_weight_map();
   }
-
-  edge_weight_map_reference get(boost::edge_weight_t,
-                                const graph&,
+  
+  edge_weight_map_reference get(edge_weight_t,
+                                const barrier_grid&,
                                 edge_weight_map_key);
-
-  inline edge_weight_map_reference get(boost::edge_weight_t tag,
-                                       const graph& g,
+  
+  inline edge_weight_map_reference get(edge_weight_t tag,
+                                       const barrier_grid& g,
                                        edge_weight_map_key e) {
     return get(tag, g)[e];
   }
@@ -93,7 +84,7 @@ namespace grid {
 
   // Euclidean heuristic for a grid
   struct euclidean_heuristic:public
-    boost::astar_heuristic<graph, edge_weight_map_reference> {
+    astar_heuristic<barrier_grid, edge_weight_map_reference> {
     euclidean_heuristic(vertex_descriptor goal):m_goal(goal) {}
 
     float operator()(vertex_descriptor v) {
@@ -106,11 +97,11 @@ namespace grid {
   // Exception thrown when the goal is found
   struct found_goal {};
   // Visitor that terminates when we find the goal
-  // template<typename graph, typename vertex_descriptor>
-  struct astar_goal_visitor:public boost::default_astar_visitor {
+  // template<typename barrier_grid, typename vertex_descriptor>
+  struct astar_goal_visitor:public default_astar_visitor {
     astar_goal_visitor(vertex_descriptor goal):m_goal(m_goal) {}
 
-    void examine_vertex(vertex_descriptor u, graph& g) {
+    void examine_vertex(vertex_descriptor u, barrier_grid& g) {
       if (u == m_goal)
         throw found_goal();
     }
--- main.cpp
+++ main.cpp
@@ -3,26 +3,26 @@
 
 
 int main(int argc, char* argv[]) {
-  using namespace grid;
+  using namespace boost;
   
   dimension_array dimensions = { {3, 4} };
-  graph g(dimensions);
+  barrier_grid g(dimensions);
 
-  // edge_descriptor e = edge_at(0, g);
-  // edge_weight_map_reference w = get(boost::edge_weight, g, e);
-  // std::cout << e << " weight " << w << std::endl;
-  // 
-  // vertex_descriptor v = vertex(0, g);
-  // std::cout << v << std::endl;
-
-  // graph::vertex_property_type x = {10, 20};
-  // graph::vertices_size_type y = 6;
-
-
+  // Edge weights are implemented.
+  edge_descriptor e = edge_at(0, g);
+  edge_weight_map_reference w = get(boost::edge_weight, g, e);
+  std::cout << e << " weight " << w << std::endl;
+  
   vertex_descriptor source = vertex(0, g), goal = vertex(3, g);
-  // boost::astar_search(g,
-  //                     source,
-  //                     euclidean_heuristic(goal),
-  //                     boost::visitor(astar_goal_visitor(goal)) );
+  std::cout << "Source " << source << "  Goal " << goal << std::endl;
+  
+  // Vertex indexes are implemented.
+  barrier_grid::vertex_index i = get(vertex_index, g)[source];
+  std::cout << source << " index " << i << std::endl;
+
+  // astar_search(g,
+  //              source,
+  //              euclidean_heuristic(goal),
+  //              visitor(astar_goal_visitor(goal)) );
   return 0;
 }
