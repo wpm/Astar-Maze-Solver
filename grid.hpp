336510ffe715189aa73a3388a23218c46d33881b
William McNeill <billmcn@gmail.com>
2010-07-04 14:20:17 -0700

    Reimplement maze Without grid_graph
    Reimplement maze from the ground up without using the grid_graph class.

--- grid.hpp
+++ grid.hpp
@@ -1,126 +1,198 @@
 #include <boost/graph/astar_search.hpp>
-// #include <boost/graph/grid_graph.hpp>
-// The local copy of grid_graph.hpp is
-// r63333 https://svn.boost.org/svn/boost/trunk/boost/graph/grid_graph.hpp
-// which fixes a bug in the released version.
-#include "grid_graph.hpp"
+#include <boost/iterator/counting_iterator.hpp>
 #include <iostream>
-#include <math.h>
+// #include <math.h>
 
+namespace maze_search {
+  // Forward declaration
+  class outgoing_edge_iterator;
 
-namespace boost {
-  #define RANK 2
-  typedef array<std::size_t, RANK> dimension_array;
-
-  // A non-wrapping rank 2 grid graph
-  struct maze: public grid_graph<RANK> {
-    maze(dimension_array& d):grid_graph<RANK>(d) {};
-  };
-
-  typedef graph_traits<maze>::vertex_descriptor vertex_descriptor;
-  typedef graph_traits<maze>::edge_descriptor edge_descriptor;
-
-
-  /*
-  Map from edges to floating point weight values
-  */
-  struct edge_weight_map {
-    typedef float value_type;
-    typedef float reference;
-    typedef edge_descriptor key_type;
-    typedef readable_property_map_tag category;
-
-    reference operator[](key_type e) const {
-      // All edges have a weight of zero.
-      return 0;
-    }
-  };
-
-
-  // ReadablePropertyGraph associated types
-  template<>
-  struct property_map<maze,
-                      edge_weight_t> {
-    typedef edge_weight_map type;
-    typedef edge_weight_map const_type;
-  };
-
-  typedef property_map<maze, edge_weight_t>::const_type
-          const_edge_weight_map;
-  typedef property_traits<const_edge_weight_map>::reference
-          edge_weight_map_reference;
-  typedef property_traits<const_edge_weight_map>::key_type
-          edge_weight_map_key;
-
-  // PropertyMap valid expressions
-  edge_weight_map_reference get(const_edge_weight_map, edge_weight_map_key);
-
-  inline edge_weight_map_reference
-  get(const_edge_weight_map pmap, edge_weight_map_key e) {
-    return pmap[e];
-  }
-
-
-  // ReadablePropertyGraph valid expressions
-  const_edge_weight_map get(edge_weight_t, const maze&);
-
-  inline const_edge_weight_map get(edge_weight_t, const maze& g) {
-    return const_edge_weight_map();
-  }
-
-  edge_weight_map_reference get(edge_weight_t,
-                                const maze&,
-                                edge_weight_map_key);
-
-  inline edge_weight_map_reference get(edge_weight_t tag,
-                                       const maze& g,
-                                       edge_weight_map_key e) {
-    return get(tag, g)[e];
-  }
-
-
-  // Euclidean heuristic for a grid
+  // A position in the maze grid.
   //
-  // This calculates the Euclidean distance between a vertex and a goal
-  // vertex.
-  struct euclidean_heuristic:public
-    astar_heuristic<maze, edge_weight_map_reference> {
-    euclidean_heuristic(vertex_descriptor goal):m_goal(goal) {}
-
-    float operator()(vertex_descriptor v) {
-      return sqrt(pow(m_goal[0]-v[0], 2) + pow(m_goal[1]-v[1], 2));
-    }
-  private:
-    vertex_descriptor m_goal;
-  };
-
-  // Exception thrown when the goal is found
-  struct found_goal {};
-
-  // Visitor that terminates when we find the goal
-  struct astar_goal_visitor:public default_astar_visitor {
-    astar_goal_visitor(vertex_descriptor goal):m_goal(m_goal) {}
-
-    // Need const otherwise we get no matching function error at:
-    // /opt/local/include/boost/graph/astar_search.hpp:141
-    void examine_vertex(vertex_descriptor u, const maze& g) {
-      if (u == m_goal)
-        throw found_goal();
-    }
-  private:
-    vertex_descriptor m_goal;
-  };
-
-
-  // Print vertices as (x, y) ordered pairs.
-  std::ostream& operator<<(std::ostream& output, const vertex_descriptor& v) {
-    output << "(" << v[0] << ", " << v[1] << ")";
+  // This is an ordered (x,y) pair.
+  class position: public std::pair<int, int> {
+  friend std::ostream& operator<<(std::ostream& output, const position& p) {
+    output << "(" << p.first << ", " << p.second << ")";
     return output;
   }
 
-  // Print edges as (x1, y1) -> (x2, y2).
-  std::ostream& operator<<(std::ostream& output, const edge_descriptor& e) {
+  public:
+    position():std::pair<int, int>(0, 0) {};
+    position(int x, int y):std::pair<int, int>(x, y) {};
+
+    position& operator+=(const position &rhs) {
+      this->first  += rhs.first;
+      this->second += rhs.second;
+      return *this;
+    }
+
+    const position operator+(const position &other) const {
+      position result = *this;
+      result += other;
+      return result;
+    }
+  };
+
+
+  // A searchable maze
+  class maze {
+  public:
+    // Graph associated types
+    typedef position vertex_descriptor;
+    typedef boost::directed_tag directed_category;
+    typedef boost::disallow_parallel_edge_tag edge_parallel_category;
+    typedef boost::vertex_list_graph_tag traversal_category;
+
+    // IncidenceGraph associated types
+    typedef std::pair<vertex_descriptor, vertex_descriptor> edge_descriptor;
+    typedef outgoing_edge_iterator out_edge_iterator;
+    typedef size_t degree_size_type;
+
+    typedef size_t vertices_size_type;
+
+
+    // Needed for graph_traits
+    typedef void adjacency_iterator;
+    typedef void in_edge_iterator;
+    typedef void vertex_iterator;
+    typedef void edge_iterator;
+    typedef void edges_size_type;
+
+    maze():m_x(0),m_y(0) {};
+    maze(vertices_size_type x, vertices_size_type y):m_x(x),m_y(y) {};
+
+    vertices_size_type x() const {return m_x;}
+    vertices_size_type y() const {return m_y;}
+
+  private:
+    size_t m_x;
+    size_t m_y;
+  };
+
+  typedef boost::graph_traits<maze>::vertex_descriptor vertex_descriptor;
+  typedef boost::graph_traits<maze>::edge_descriptor edge_descriptor;
+  typedef boost::graph_traits<maze>::out_edge_iterator out_edge_iterator;
+  typedef boost::graph_traits<maze>::degree_size_type degree_size_type;
+
+  // Tag values passed to an iterator constructor to specify whether it should
+  // be created at the start or the end of its range.
+  struct iterator_position {};
+  struct iterator_start:virtual public iterator_position {};
+  struct iterator_end:virtual public iterator_position {};
+
+
+  // IncidenceGraph
+  
+  // Iterator over adjacent vertices in a grid
+  class outgoing_edge_iterator:public boost::iterator_adaptor <
+      outgoing_edge_iterator,
+      boost::counting_iterator<size_t>,
+      edge_descriptor,
+      boost::use_default,
+      edge_descriptor >
+  {
+  public:
+    outgoing_edge_iterator():
+      outgoing_edge_iterator::iterator_adaptor_(4),
+      m_maze(NULL),m_u(position()) {};
+    explicit outgoing_edge_iterator(const maze& m,
+                                    vertex_descriptor u,
+                                    iterator_start):
+      outgoing_edge_iterator::iterator_adaptor_(0),
+      m_maze(&m),m_u(u) {
+        find_next_valid_offset();
+      };
+    explicit outgoing_edge_iterator(const maze& m,
+                                    vertex_descriptor u,
+                                    iterator_end):
+      outgoing_edge_iterator::iterator_adaptor_(4),
+      m_maze(&m),m_u(u) {
+        find_next_valid_offset();
+      };
+
+  private:
+    friend class boost::iterator_core_access;
+
+    void increment() {
+      this->base_reference()++;
+      find_next_valid_offset();
+    }
+
+    edge_descriptor dereference() const {
+      return edge_descriptor(m_u, m_u + current_offset());
+    }
+
+    void find_next_valid_offset() {
+      while (off_grid() && *this->base_reference() < 4)
+        this->base_reference()++;
+    }
+
+    position current_offset () const {
+      static const position offset[] = {
+        position(0, -1),
+        position(1, 0),
+        position(0, 1),
+        position(-1, 0)};
+      return offset[*this->base_reference()];
+    }
+
+    bool off_grid() {
+      position v = m_u + current_offset();
+      if (v.first < 0 || v.first == m_maze->x())
+        return true;
+      if (v.second < 0 || v.second == m_maze->y())
+        return true;
+      return false;
+    }
+
+    const maze *m_maze;
+    vertex_descriptor m_u; // Vertex whose out edges are iterated
+  };
+
+  
+  vertex_descriptor source(edge_descriptor, const maze&);
+
+  inline vertex_descriptor
+  source(edge_descriptor e, const maze&) {
+    // The first vertex in the edge pair is the source.
+    return e.first;
+  }
+
+  vertex_descriptor target(edge_descriptor, maze&);
+
+  inline vertex_descriptor
+  target(edge_descriptor e, const maze&) {
+   // The second vertex in the edge pair is the target.
+   return e.second;
+  }
+
+  std::pair<out_edge_iterator, out_edge_iterator>
+  out_edges(vertex_descriptor, const maze&);
+
+  inline std::pair<out_edge_iterator, out_edge_iterator>
+  out_edges(vertex_descriptor u, const maze& m) {
+    return std::pair<out_edge_iterator, out_edge_iterator>(
+      out_edge_iterator(m, u, iterator_start()),
+      out_edge_iterator(m, u, iterator_end()) );
+  }
+
+  degree_size_type out_degree(vertex_descriptor, const maze&);
+
+  inline degree_size_type out_degree(vertex_descriptor u, const maze& m) {
+    degree_size_type d = 4;
+
+    if (u.first == 0 || u.first == m.x())
+      d -= 1;
+    if (u.second == 0 || u.second == m.y())
+      d -= 1;
+    return d;
+  }
+
+
+  inline std::ostream& operator<<(std::ostream& output,
+                                  const edge_descriptor& e) {
     output << e.first << " -> " << e.second;
     return output;
   }
+
 }
--- main.cpp
+++ main.cpp
@@ -1,36 +1,17 @@
-#include <iostream>
 #include "grid.hpp"
 
 
 int main(int argc, char* argv[]) {
-  using namespace boost;
+  using namespace maze_search;
 
-  function_requires< ReadablePropertyMapConcept<const_edge_weight_map,
-                                                edge_descriptor> >();
-  function_requires< ReadablePropertyGraphConcept<maze,
-                                                  edge_descriptor,
-                                                  edge_weight_t> >();
-  function_requires< VertexListGraphConcept<maze> >();
-  function_requires< AStarHeuristicConcept<euclidean_heuristic,
-                                           maze> >();
-
-  dimension_array dimensions = { {3, 4} };
-  maze g(dimensions);
-  vertex_descriptor source = vertex(0, g), goal = vertex(3, g);
-  // astar_search(g,
-  //              source,
-  //              euclidean_heuristic(goal),
-  //              visitor(astar_goal_visitor(goal)) );
-
-  // Edge weights are implemented.
-  const_edge_weight_map edge_map = get(edge_weight, g);
-  edge_descriptor e = edge_at(0, g);
-  edge_weight_map_reference w = edge_map[e];
-  std::cout << e << " weight " << w << std::endl;
-
-  // Vertex indexes are implemented.
-  std::cout << source << " index " << get(vertex_index, g)[source]
-            << std::endl;
-
+  maze m(3,3);
+  position u(1,1);
+  
+  out_edge_iterator ei, ei_end;
+  for(tie(ei, ei_end) = out_edges(u, m); ei != ei_end; ei++) {
+    edge_descriptor  e = *ei;
+    std::cout << e << std::endl;
+  }
+  
   return 0;
 }
